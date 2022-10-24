0c1dd1fb32599662f4af3566a8cd81b4b453a226
vintagehsieh <vintagehsieh@gmail.com>
2022-10-24 17:02:08 +0800

    加入專案檔案。

---
+++ Exec_While01_Q1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_While01_Q1
+{
+	internal class Program
+	{
+		/// <summary>
+		/// Exec_While01
+		/// Q1: 1+ 2 + 3 + 4 + .... + N < 105
+		/// 請問 N 是多少?
+		/// </summary>
+
+		static void Main(string[] args)
+		{
+			int sum = 0;
+			int i = 1;
+
+			do
+			{
+				sum += i;
+				i++;
+			} while (sum < 105);
+
+			if (sum >= 105)
+			{
+				Console.WriteLine($"N值為{i - 1}");
+			}
+			else
+			{
+				Console.WriteLine($"N值為{i}");
+			}
+		}
+	}
+}
---
+++ Exec_While01_Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_While01_Q2
+{
+	internal class Program
+	{
+		/// <summary>
+		/// Q2: 使用 while , 在畫面顯示所有可以整除 300 的整數
+		/// 例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+		/// </summary>
+
+		static void Main(string[] args)
+		{
+			int n = 0;
+
+			Console.WriteLine("可以整除300的數字有: ");
+			while ( n < 300)
+			{
+				n++;
+				if ( 300 % n ==0)
+				Console.WriteLine(n);
+			}
+		}
+	}
+}
